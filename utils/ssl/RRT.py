eea44e115d5b69c51e2585f4533aecc7ceffc974
xupenio04 <pedrocampello17@gmail.com>
2025-01-09 14:16:28 -0300

    fix: creating RRT file in the ssl folder for organization

--- utils/ssl/RRT.py
+++ utils/ssl/RRT.py
@@ -1,5 +1,3 @@
-from utils.ssl.Navigation import Navigation
-from utils.ssl.base_agent import BaseAgent
 import numpy as np
 from utils.Point import Point
 import random
@@ -39,23 +37,3 @@ class RRT:
     
     def nearest(self, point):
         return min(self.tree.keys(), key=lambda p: self.distance(p, point))
-    
-
-    
-
-class ExampleAgent(BaseAgent):
-    def __init__(self, id=0, yellow=False):
-        super().__init__(id, yellow)
-
-    def decision(self):
-        if len(self.targets) == 0:
-            return
-
-        target_velocity, target_angle_velocity = Navigation.goToPoint(self.robot, self.targets[0])
-        self.set_vel(target_velocity)
-        self.set_angle_vel(target_angle_velocity)
-
-        return
-
-    def post_decision(self):
-        pass
